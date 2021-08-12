6d2e7ff91e834dfe3f6614418a38ddc15b5a552d
Nalini Singh <nmsingh@mit.edu>
2021-08-12 14:37:37 -0400

    Implement faster contrastive loss
    Contrastive loss implementation with stored multiplications instead of for loops.
    Also fixes a few syntax bugs.

--- model/loss.py
+++ model/loss.py
@@ -2,10 +2,42 @@
 import torch
 from torch.nn import functional as F
 
-from model.utils import euclidean_dist
+#from model.utils import euclidean_dist
 
 
-def loss_task(encoded, prototypes, target, criterion='dist'):
+def contrastive_loss(encoded, encoded_aug, temp=0.07):
+    assert(encoded.size()==encoded_aug.size())
+
+    cl_total = 0
+    encoded_norm = F.normalize(encoded, p=2, dim = 1)
+    encoded_aug_norm = F.normalize(encoded_aug, p=2, dim=1)
+
+    aug_batch = torch.cat([encoded_norm, encoded_aug_norm], dim=0)
+    n = aug_batch.size()[0]
+
+    for i in range(n):
+        # Get the index of the pair
+        j = (i+(n//2)) % n 
+
+        cl_i_num = torch.exp(torch.dot(aug_batch[i,:],aug_batch[j,:])/temp)
+        cl_i_den = 0
+
+        for k in range(n):
+            if(i!=k):
+                cl_i_den += torch.exp(torch.dot(aug_batch[i,:],aug_batch[k,:])/temp)
+        
+        cl_total += -torch.log(cl_i_num/cl_i_den)
+        print(cl_i_num/cl_i_den)
+        print(cl_total)
+    return cl_total
+
+batch = torch.ones([4,10])
+batch_aug = torch.ones([4,10])*2
+
+print(contrastive_loss(batch,batch_aug))
+
+
+def loss_task(encoded, prototypes, target, criterion='dist', encoded_augment=None):
     """Calculate loss.
     criterion: NNLoss - assign to closest prototype and calculate NNLoss
          dist - loss is distance to prototype that example needs to be assigned to
@@ -37,7 +69,10 @@ def loss_task(encoded, prototypes, target, criterion='dist'):
         y_hat = torch.max(-dists,1)[1]
         
     acc_val = y_hat.eq(target.squeeze()).float().mean()    
-        
+    
+    if(encoded_augment is not None):
+        loss_val += contrastive_loss(encoded, encoded_augment)
+
     return loss_val, acc_val
 
 def loss_test_nn(encoded, prototypes):
@@ -79,7 +114,7 @@ def loss_test_basic(encoded, prototypes):
     
     return loss_val, args_count
 
-def loss_test(encoded, prototypes, tau):
+def loss_test(encoded, prototypes, tau, encoded_augment):
     #prototypes = torch.stack(prototypes).squeeze() 
     loss_val_test, args_count = loss_test_basic(encoded, prototypes)
     
@@ -90,6 +125,8 @@ def loss_test(encoded, prototypes, tau):
         
         loss_val_test += tau*loss_val2
         
+    if(encoded_augment):
+        loss_val_test += contrastive_loss(encoded, encoded_augment)
     return loss_val_test, args_count
 
 def reconstruction_loss(decoded, x):
